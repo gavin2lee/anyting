e1ed430955000f6f98b0c2e1e9e1fee83a33e7a1
gavin2lee <gavin2lee@163.com>
2016-09-26 15:46:37 +0800

    controller

--- src/main/java/com/gl/anything/controller/HelloWorldController.java
+++ src/main/java/com/gl/anything/controller/HelloWorldController.java
@@ -2,21 +2,30 @@ package com.gl.anything.controller;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gl.anything.vo.GreetingVo;
 
+@Controller
 @RequestMapping("/helloservice")
 public class HelloWorldController {
-	
+	private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
+
 	@RequestMapping("/greeting")
-	public GreetingVo simplyGreeting(@RequestParam String username){
+	@ResponseBody
+	public GreetingVo simplyGreeting(@RequestParam String username) {
+		LOG.debug("simplyGreeting : " + username);
+
 		GreetingVo vo = new GreetingVo();
 		vo.setId(100);
 		vo.setGreetAt(new Date());
-		vo.setWords("Hi "+username);
-		
+		vo.setWords("Hi " + username);
+
 		return vo;
 	}
 }
