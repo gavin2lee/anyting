b3338fd4c49445fcd641208ba1b56d42279ece1f
gavin2lee <gavin2lee@163.com>
2016-09-28 14:50:21 +0800

    mybatis

---
+++ etc/dbtemplate/insert-table.sql
@@ -0,0 +1,51 @@
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'A', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'B', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'C', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'D', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'E', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'F', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'G', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'H', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'I', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'J', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'K', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'L', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'M', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'N', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'O', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'P', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'Q', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'R', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'S', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'T', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'U', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'V', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'W', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'X', 10, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'Y', 11, PARSEDATETIME('24-10-2014 13:34:00', 'dd-MM-yyyy HH:mm:ss'), '1');
+
+insert into T_ORDER (ID, CLIENTNAME, AMOUNT, CREATETIME, SEX) values (seq_order.nextval, 'Z', 12, PARSEDATETIME('24-10-2014 13:34:07', 'dd-MM-yyyy HH:mm:ss'), '1');
